7150ab08f0e99558442d2041e4163d54ea5ed54c
Felipe Moreno <felipe@flpm.dev>
2024-06-16 11:46:24 -0400

    final updates on nox

--- noxfile.py
+++ noxfile.py
@@ -39,13 +39,13 @@ AUTOBUILD_INCLUDE = [
     pathlib.Path("_static", "pyos.css")
 ]
 
-## Internationalization options (translations)
+## Localization options (translations)
 
 # List of languages for which locales will be generated in (/locales/<lang>)
-LANGUAGES = ["es"]
+LANGUAGES = ["es", "pt"]
 
-# List of languages to build, a subset of LANGUAGES including the language translations that are ready to publish
-EXCLUDE_FROM_BUILD_LANGUAGES = []
+# List of languages that should be built when releasing the guide
+RELEASE_LANGUAGES = ['es']
 
 
 @nox.session
@@ -53,34 +53,63 @@ def docs(session):
     """Build the packaging guide."""
     session.install("-e", ".")
     session.run(SPHINX_BUILD, *BUILD_PARAMETERS, SOURCE_DIR, OUTPUT_DIR, *session.posargs)
-    # When building the guide, also build the translations
-    session.notify("translation-build")
+    # When building the guide, also build the translations in RELEASE_LANGUAGES
+    session.notify("build-translations", ['release-build'])
+
 
 @nox.session(name="docs-test")
 def docs_test(session):
-    """Build the packaging guide with additional parameters."""
+    """
+    Build the packaging guide with more restricted parameters.
+
+    Note: this is the session used in CI/CD to release the guide.
+    """
     session.install("-e", ".")
     session.run(SPHINX_BUILD, *BUILD_PARAMETERS, *TEST_PARAMETERS, SOURCE_DIR, OUTPUT_DIR, *session.posargs)
-    # When building the guide with testing parameters, also build the translations with those parameters
-    session.notify("translation-build", TEST_PARAMETERS)
+    # When building the guide with additional parameters, also build the translations in RELEASE_LANGUAGES
+    # with those same parameters.
+    session.notify("build-translations", ['release-build', *TEST_PARAMETERS])
 
-# This session will build the guide in English, to use it with a translation during development,
-# pass the language as a parameter to sphinx-build via nox with (example for Spanish):
-# nox -s docs-live -- -D language=es
+
 @nox.session(name="docs-live")
 def docs_live(session):
-    """Build and launch a local copy of the guide."""
+    """
+    Build and launch a local copy of the guide.
+
+    This session will use sphinx-autobuild to build the guide and launch a local server so you can
+    browse it locally. It will automatically rebuild the guide when changes are detected in the source.
+
+    It can be used with the language parameter to build a specific translation, for example:
+
+        nox -s docs-live -- -D language=es
+
+    Note: The docs-live-lang session below is provided as a convenience session for beginner contributors
+    so they don't need to remember the specific sphinx-build parameters to build a different language.
+    """
     session.install("-e", ".")
     cmd = [SPHINX_AUTO_BUILD, *BUILD_PARAMETERS, SOURCE_DIR, OUTPUT_DIR, *session.posargs]
     for folder in AUTOBUILD_IGNORE:
         cmd.extend(["--ignore", f"*/{folder}/*"])
-    # for folder in AUTOBUILD_INCLUDE: # This was commented in the original nox file
+    # This part was commented in the previous version of the nox file, keeping the same here
+    # for folder in AUTOBUILD_INCLUDE:
     #     cmd.extend(["--watch", folder])
     session.run(*cmd)
 
+
 @nox.session(name="docs-live-lang")
 def docs_live_lang(session):
-    """A convenience session for beginner contributors to use docs-live with a language translation."""
+    """
+    A convenience session for beginner contributors to use the docs-live session with
+    a specific language.
+
+    It expects the language code to be passed as the first positional argument, so it needs
+    to be called with from the command line with the following syntax:
+
+        nox -s docs-live-lang -- LANG
+
+    where LANG is one of the available languages defined in LANGUAGES.
+    For example, for Spanish: nox -s docs-live-lang -- es
+    """
     if not session.posargs:
         session.error(
             "Please provide a language using:\n\n      "
@@ -98,6 +127,7 @@ def docs_live_lang(session):
             f"where LANG is one of: {LANGUAGES}"
         )
 
+
 @nox.session(name="docs-clean")
 def clean_dir(session):
     """Clean out the docs directory used in the live build."""
@@ -110,25 +140,75 @@ def clean_dir(session):
         else:
             os.remove(content)
 
-@nox.session(name="translation-update")
-def translation_update(session):
-    """Update the templates (.pot), and translation files (.po) for each of the languages."""
+
+@nox.session(name="update-translations")
+def update_translations(session):
+    """
+    Update the translation files (./locales/*/.po) for all languages translations.
+
+    Note: this step is important because it makes sure that the translation files are
+    up to date with the latest changes in the guide.
+    """
     session.install("-e", ".")
     session.install("sphinx-intl")
-    session.log("Updating gettext templates (.pot)")
+    session.log("Updating templates (.pot)")
     session.run(SPHINX_BUILD, *TRANSLATION_TEMPLATE_PARAMETERS, SOURCE_DIR, TRANSLATION_TEMPLATE_DIR, *session.posargs)
     for lang in LANGUAGES:
         session.log(f"Updating .po files for [{lang}] translation")
         session.run("sphinx-intl", "update", "-p", TRANSLATION_TEMPLATE_DIR, "-l", lang)
 
-@nox.session(name="translation-build")
-def translation_build(session):
-    """Build the translations of the guide"""
-    BUILD_LANGUAGES = [lang for lang in LANGUAGES if lang not in EXCLUDE_FROM_BUILD_LANGUAGES]
-    if not BUILD_LANGUAGES:
-        session.warn("No languages specified for translation build")
-        return
+
+@nox.session(name="build-languages")
+def build_languages(session):
+    """
+    Build the translations of the guide for the specified language.
+
+    Note: This sessions expects a list of languages to build in the first position of the session arguments.
+    It does not need to be called directly, it is started by build_translations session.
+    """
+    if not session.posargs:
+        session.error("Please provide the list of languages to build the translation for")
+    languages_to_build = session.posargs.pop(0)
+
     session.install("-e", ".")
-    for lang in BUILD_LANGUAGES:
+    for lang in languages_to_build:
+        if lang not in LANGUAGES:
+            session.warn(f"Language [{lang}] is not available for translation")
+            continue
         session.log(f"Building [{lang}] guide")
         session.run(SPHINX_BUILD, *BUILD_PARAMETERS, "-D", f"language={lang}", ".", OUTPUT_DIR / lang, *session.posargs)
+
+
+@nox.session(name="build-translations")
+def build_translations(session):
+    """
+    Build translations of the guide.
+
+    Note: this session can be called directly to build all available translations (defined in LANGUAGES).
+    It is also called by the docs and docs-test sessions with 'release-build' as the first positional
+    argument, to build only the translations defined in RELEASE_LANGUAGES.
+    """
+    release_build = False
+    if session.posargs and session.posargs[0] == 'release-build':
+        session.posargs.pop(0)
+        release_build = True
+    # if running from the docs or docs-test sessions, build only release languages
+    BUILD_LANGUAGES = RELEASE_LANGUAGES if release_build else LANGUAGES
+    session.log(f"Existing translations: {LANGUAGES}")
+    session.log(f"Release Languages: {RELEASE_LANGUAGES}")
+    session.log(f"Building languages{' for release' if release_build else ''}: {BUILD_LANGUAGES}")
+    if not BUILD_LANGUAGES:
+        session.warn("No translations to build")
+    else:
+        session.notify("build-languages", [BUILD_LANGUAGES, *session.posargs])
+
+
+@nox.session(name="build-translations-test")
+def build_translations_test(session):
+    """
+    Build all translations of the guide with testing parameters.
+
+    This is a convenience session to test the build of all translations with the testing parameters
+    in the same way docs-test does for the English version.
+    """
+    session.notify("build-translations", [*TEST_PARAMETERS])
